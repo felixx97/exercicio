9990850d3e232bb4ab89b1a7845e481f3375c9e9
Abraão Félix <83102473+felixx97@users.noreply.github.com>
2022-03-03 17:31:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+alert("digital iniva" + formacoes)
+
+
+
+/*var frutas = [ {nome: "pera", cor: "Azul"}, {nome: "açaí", cor: "roxo"} ];
+console.log(frutas);
+alert(frutas[1].mome);*/
+
+
+
+/*var lista = ["MAÇA", "pera", "laranja"]
+lista.push("uva")
+
+console.log(lista.length)
+console.log(lista.join(' - '))*/
+
+
+/*var nome = "pablo";
+var n1 = 22;
+var n2 = 22;
+var frase = "O Japão é o melhor time do mundo";
+
+console.log(nome)
+console.log(n1 + n2);
+console.log(frase.replace("Japão", "Brasil"))
+*/
